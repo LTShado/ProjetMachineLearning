0be9d89c388d0880d3e90440f016895a3bb520ca
François Bouscaillou <francois.bouscaillou@gmail.com>
2023-01-06 22:55:46 +0100

    feature : create class PMC with construct

--- Lib/PMC.cpp
+++ Lib/PMC.cpp
@@ -1,5 +1,50 @@
 #include "PMC.hpp"
 
+PMC::~PMC() {}
+
 PMC::PMC() {}
 
-PMC::~PMC() {}
+PMC::PMC(vector<int> npl)
+{
+    L = npl.size() - 1;
+
+    for (int k = 0; k < npl.size(); k++)
+        d.push_back(npl[k]);
+
+    // initialisation des W:
+    for (int l = 0; l < d.size(); l++)
+    {
+        // W.push_back();
+        if (l == 0)
+            continue;
+        for (int i = 0; i < d[l - 1] + 1; i++)
+        {
+            // W[l].push_back();
+            for (int j = 0; j < d[l] + 1; j++)
+            {
+                // W[l][i].push_back(j == 0 ? 1 : 0);
+            }
+        }
+    }
+
+    // initialisation des X et des deltas:
+    for (int l = 0; l < d.size(); l++)
+    {
+        // X.push_back();
+        // deltas.push_back();
+        for (int j = 0; j < d[l] + 1; j++)
+        {
+            // deltas[l].push_back(0);
+            // X[l].push_back(j == 0 ? 1 : 0);
+        }
+    }
+}
+
+void PMC::propagate(vector<float> inputs, bool isClassification) {}
+
+vector<float> PMC::predict(vector<float> inputs, bool isClassification)
+{
+    return vector<float>();
+}
+
+void PMC::train(vector<vector<float>> xTrain, vector<vector<float>> yTrain, bool isClassification, float alpha, int nbIter) {}
--- Lib/PMC.hpp
+++ Lib/PMC.hpp
@@ -1,9 +1,24 @@
+#include <vector>
+
+using namespace std;
+
 #pragma once
 
 class PMC
 {
 private:
+    int L;
+    vector<int> d;
+    vector<vector<int>> X;
+    vector<vector<int>> deltas;
+    vector<vector<vector<int>>> W;
+
 public:
-    PMC();
     ~PMC();
+    PMC();
+    PMC(vector<int> npl);
+
+    void propagate(vector<float> inputs, bool isClassification);
+    vector<float> predict(vector<float> inputs, bool isClassification);
+    void train(vector<vector<float>> xTrain, vector<vector<float>> yTrain, bool isClassification, float alpha = 0.01, int nb_iter = 10000);
 };
